9bc22c2be9863e7f665178ec1bc9608357e50c50
Nataliia Sukhinina <nataliia.sukhinina@gmail.com>
2024-10-08 13:19:13 -0500

    Software camera

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { TimerComponent } from './timer/timer.component';
+import { SoftwareCameraComponent } from './software-camera/software-camera.component';
 
 export const routes: Routes = [
-    { path: 'timer', component: TimerComponent}
+  { path: 'timer', component: TimerComponent },
+  { path: 'software-camera', component: SoftwareCameraComponent },
 ];
